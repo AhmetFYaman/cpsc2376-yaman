6e7a644262d47f8b036962a9ac8b1ba2381b28a9
AhmetFYaman <Afyaman@ualr.edu>
2025-01-30 13:35:40 -0600

    yeye

--- main.cpp
+++ main.cpp
@@ -8,20 +8,42 @@ int main(){
 	nums.push_back(12);
 	nums.push_back(17);
 	nums.push_back(90);
-	int x{ 0 };
+	int user_vector{0};
+
+	int sum{0};
     
     std::cout << "Gimme num for my vector \n";
-	x = getValidInteger();
+	user_vector = getValidInteger();
 
-	nums.push_back(x);
+	nums.push_back(user_vector);
 	
 	std::cout << "My nums in my vector: \n";
 
 
 	for (int num : nums){
-		std::cout << num << " \n";
+		std::cout << "original: " << num << " \n";
+		num = num*2;
+		}
+
+	for (int num : nums){
+		std::cout << "*new*: doubled: " << num << " \n";
+		sum += num;
+		}
+		std::cout << "the total of this new vector: " << sum << ": ";
+	
+	std::cout << "GImme a number, I will find multiples and fiond vectors: " << std::endl; 
+	int multiple{0};
+
+	std::vector<int> multiples;
+	multiple = getValidInteger();
+
+	for (int i = 1; i < 11; i++){
+		multiples.push_back(multiple*i);
+		std::cout << multiples.at(i-1);
 	}
 
+	multiple = getValidInteger();
+	
 
 	return 0;
  }
